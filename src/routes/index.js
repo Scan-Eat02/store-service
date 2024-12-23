bddbda12d859720d31219dcfae350f0647653433
yashd26 <yashd026@gmail.com>
2024-12-23 15:08:47 +0530

    update: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ const { serviceEndpointPrefix } = require("../config");
 router.post(`${serviceEndpointPrefix}/create-store`, (req, res) => storeController.createStoreAction(req, res));
 router.delete(`${serviceEndpointPrefix}/delete-store`, (req, res) => storeController.deleteStoreAction(req, res));
 router.put(`${serviceEndpointPrefix}/update-store`, (req, res) => storeController.updateStoreAction(req, res));
-router.put(`${serviceEndpointPrefix}/get-admin-stores`, (req, res) => storeController.getUserStoresAction(req, res));
-router.put(`${serviceEndpointPrefix}/get-all-stores`, (req, res) => storeController.getAllStoresAction(req, res));
+router.get(`${serviceEndpointPrefix}/get-admin-stores`, (req, res) => storeController.getUserStoresAction(req, res));
+router.get(`${serviceEndpointPrefix}/get-all-stores`, (req, res) => storeController.getAllStoresAction(req, res));
 
 module.exports = router;
